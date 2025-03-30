dd6893ff30b6f4f5ee1779bfe1895fa4ae4d6023
Kavix28 <kavyakapoor28i@gmail.com>
2025-03-30 23:47:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Handle form submission (e.g., send data to the server)
+    alert('Form submitted!');
+});
